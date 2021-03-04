04fc45f04de43e853d2a3f55e224779109cd65d3
umustdye <heidibdye@gmail.com>
2021-03-04 01:43:23 -0800

    updated tests for iterator

--- src/iteratorTest.hpp
+++ src/iteratorTest.hpp
@@ -10,44 +10,120 @@
 #include "base.hpp"
 #include "add.hpp"
 #include <iostream>
-TEST(iteratorTest, constructor)
+TEST(iteratorTest, traversals)
 {
     Base* three = new Op(3);
     Base* seven = new Op(7);
     Base* four = new Op(4);
     Base* two = new Op(2);
     Base* add = new Add(three, seven);
-    Base* add2 = new Add(add, four);
-    //Base* mult = new Mult(seven, four);
-    //Base* add = new Add(mult, two);
-    //std::cout<<add2->number_of_children()<<std::endl;
-    Iterator* it = new Iterator(add2);
-    std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-    std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-    std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-    std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-    std::cout<<"num child: "<<it->current_node()->number_of_children()<<std::endl;
-std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-
-std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-
- std::cout<<it->current_node()->stringify()<<std::endl;
-    it->next();
-
-    //Base* currentNode = it->current_node();
-    //std::cout<<currentNode->stringify()<<std::endl;
-//    std::cout<<it->current_index()<<std::endl;
-    //std::cout<<it->current_node.stringify()<<std::endl;
+    Base* mult = new Mult(add, four);
+    Iterator* it = new Iterator(mult);
+    int traversals = 0;
+    while(!it->is_done())
+    {
+        //std::cout<<it->current_node()->stringify()<<std::endl;
+        it->next();
+        traversals++;
+    }
+    EXPECT_EQ(traversals, 9);
 }
+
+TEST(iteratorTest, op)
+{
+    Base* four = new Op(4);
+    Iterator* it = new Iterator(four);
+    EXPECT_EQ(it->current_node()->stringify(), "4");
+    EXPECT_EQ(it->current_index(), 0);
+    EXPECT_EQ(it->current_node()->number_of_children(), 0);
+}
+
+
+TEST(iteratorTest, correctValues)
+{
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* add = new Add(three, seven);
+    Base* mult = new Mult(add, four);
+    Iterator* it = new Iterator(mult);
+
+    EXPECT_EQ(it->current_node()->stringify(), mult->stringify());
+    it->next();
+    
+    EXPECT_EQ(it->current_node()->stringify(), add->stringify());
+    it->next();
+
+    EXPECT_EQ(it->current_node()->stringify(), three->stringify());
+    it->next();
+
+    EXPECT_EQ(it->current_node()->stringify(), add->stringify());
+    it->next();
+
+    EXPECT_EQ(it->current_node()->stringify(), seven->stringify());
+    it->next();
+
+    EXPECT_EQ(it->current_node()->stringify(), add->stringify());
+    it->next();
+
+    EXPECT_EQ(it->current_node()->stringify(), mult->stringify());
+    it->next();
+
+    EXPECT_EQ(it->current_node()->stringify(), four->stringify());
+    it->next();
+
+
+    EXPECT_EQ(it->current_node()->stringify(), mult->stringify());
+    it->next();
+
+}
+
+
+TEST(iteratorTest, index)
+{
+    Base* three = new Op(3);
+    Base* seven = new Op(7);
+    Base* four = new Op(4);
+    Base* two = new Op(2);
+    Base* add = new Add(three, seven);
+    Base* mult = new Mult(add, four);
+    Iterator* it = new Iterator(mult);
+
+    EXPECT_EQ(it->current_index(), 0);
+    it->next();
+    
+    EXPECT_EQ(it->current_index(), 0);
+    it->next();
+
+    EXPECT_EQ(it->current_index(), 0);
+    it->next();
+
+    EXPECT_EQ(it->current_index(), 1);
+    it->next();
+
+    EXPECT_EQ(it->current_index(), 0);
+    it->next();
+
+    EXPECT_EQ(it->current_index(), 2);
+    it->next();
+
+    EXPECT_EQ(it->current_index(), 1);
+    it->next();
+
+    EXPECT_EQ(it->current_index(), 0);
+    it->next();
+
+
+    EXPECT_EQ(it->current_index(), 2);
+    it->next();
+
+
+
+
+
+}
+
+
 
 #endif //__ITERATOR_TEST_HPP__
